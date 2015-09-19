afe6f54f0bb702901894f783b6522607117edcd3
Ian Macalinao <me@ian.pw>
2015-09-19 15:12:05 -0400

    fix public path

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from 'express';
 
 const app = express();
 
-app.use(express.static(`${__dirname}public/`));
+app.use(express.static(`${__dirname}/public/`));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
