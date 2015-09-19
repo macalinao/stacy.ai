644bf8ac5e20cbc13dbddc7ec0257384323b7b05
Ian Macalinao <me@ian.pw>
2015-09-19 15:10:26 -0400

    Add public dir

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.static(`${__dirname}public/`));
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
