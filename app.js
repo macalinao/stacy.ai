a5a00939aeacdacba8b676a3ea6765a58fd594b7
Ian Macalinao <me@ian.pw>
2015-09-19 15:34:20 -0400

    Assets in separate dir

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import express from 'express';
 const app = express();
 
 app.use(express.static(`${__dirname}/public/`));
+app.use(`/assets`, express.static(`${__dirname}/assets/`));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
