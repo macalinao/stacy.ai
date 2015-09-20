5c991aaf0dfa083dae3402a835c773ccf80dc67c
abhyvyth <abhyvyth@gmail.com>
2015-09-20 01:58:50 -0700

    uber yay

--- app.js
+++ app.js
@@ -26,52 +26,6 @@ server.listen(PORT, () => {
 });
 
 
-//get lat long
-app.get('/google', function(req,res){
-request('http://maps.googleapis.com/maps/api/geocode/json?address=Bangalore&sensor=false',
-       function(error,response,body){
-               if (!error && response.statusCode == 200) {
-                       var result = JSON.parse(body).results;
-                       var address=result[0].geometry.location;//this is an object with lat, lng
-                       res.send(address);
-               }
-       });
-});
-
-//hotel code//
-function hotelNameAndPrice(checkin, checkout, langtitude, longtitude){//date must be in form yyyy-mm-dd
-    var url = "http://api.sandbox.amadeus.com/v1.2/hotels/search-circle?latitude="+langtitude+"&longitude="+longtitude+"&radius=50&check_in="+checkin+"&check_out="+checkout+"&chain=RT¤cy=EUR&number_of_results=50&apikey=5O8rga7DF6pJAnARH4b18YfJvF8nieSO";
-    request(url, function(error,response,body){
-            if(!error && response.statusCode == 200) {
-                    var results = JSON.parse(body).results[0];
-                    var property_name = results.property_name; //name of hotel;
-                    var lat = results.location.latitude; //lat
-                    var lng = results.location.longitude; //lng
-                    var price = results.total_price.amount;
-                    return {name: property_name, lat: lat, lng: lng, price: price};
-             }
-    });
-}
-
-
-
-
-
-
-//get airport code
-app.get('/airport', function(req,res){
-request('http://api.sandbox.amadeus.com/v1.2/airports/nearest-relevant?apikey=5O8rga7DF6pJAnARH4b18YfJvF8nieSO&latitude=46.6734&longitude=-71.7412', 
-       function(error,response,body){
-               if (!error && response.statusCode == 200) {
-               		   console.log(response);
-                       var result = JSON.parse(body)[0].airport;
-         
-                       res.send(result);
-               }
-       });
-});
-
-
 
 //get uber product
 app.get('/uber', function(req,res){
@@ -85,6 +39,7 @@ request('https://api.uber.com/v1/products?latitude=37.7759792&longitude=-122.418
        });
 });
 
+//help ian 
 // //get request id
 // app.get('/uberRequestId', function(req,res){
 // request.post('https://api.uber.com/v1/requests/start_latitude=37.334381&start_longitude=-121.89432&end_latitude=37.77703&end_longitude=-122.419571&product_id=a1111c8c-c720-46c3-8534-2fcdd730040d&server_token=YliT_3eRG-sh1mttNF97FGbqWnEXu4HuyCIWiQnB', 
