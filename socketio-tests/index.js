e4b2cebcd6cd34c50fe33edd64f202be297c80e1
Marie Bremner <marie@Maries-MacBook-Air.local>
2015-09-19 22:31:46 -0400

    slowly parsing info out

--- socketio-tests/index.js
+++ socketio-tests/index.js
@@ -1,18 +1,34 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var request = require('request');
+
+          
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 app.get('/marie', function(req,res){
-    res.send('marie');
+request('http://api.sandbox.amadeus.com/v1.2/flights/low-fare-search?origin=IST&destination=BOS&departure_date=2015-10-15&return_date=2015-10-21&number_of_results=3&apikey=5O8rga7DF6pJAnARH4b18YfJvF8nieSO', 
+       function(error,response,body){
+               if (!error && response.statusCode == 200) {
+                       var result = JSON.parse(body).results;
+                       var itineraries = result[0].itineraries[0];
+                       var outbound = itineraries.outbound;
+                       var outbound_flight_1 = outbound.flights[0];
+                       var outbound_flight_2 = outbound.flights[1];
+                       
+                       var inbound = itineraries.inbound; 
+                       res.send(outbound_flight_2);
+               }
+       });
 });
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
-    io.emit('chat message', "abhy: "+msg);
+    io.emit('chat message', "user_name: "+msg);
+    
     console.log('message'+ msg);
   });
 });
