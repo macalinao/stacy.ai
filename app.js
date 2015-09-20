77144f33ee4c358045e4ba4fa66bcea7d71862da
Ian Macalinao <me@ian.pw>
2015-09-20 03:22:09 -0400

    it runs again

--- app.js
+++ app.js
@@ -7,9 +7,6 @@ import ai from './lib/ai';
 const app = express();
 const server = http.Server(app);
 const io = socketIo(server);
-const redisStore = connectRedis(express);
-const sessionStore = new redisStore();
-const cookieParser = express.cookieParser('ian');
 
 app.use(express.static(`${__dirname}/public/`));
 app.use(`/assets`, express.static(`${__dirname}/assets/`));
@@ -45,14 +42,14 @@ function hotelNameAndPrice(checkin, checkout, langtitude, longtitude){//date mus
     var url = "http://api.sandbox.amadeus.com/v1.2/hotels/search-circle?latitude="+langtitude+"&longitude="+longtitude+"&radius=50&check_in="+checkin+"&check_out="+checkout+"&chain=RT¤cy=EUR&number_of_results=50&apikey=5O8rga7DF6pJAnARH4b18YfJvF8nieSO";
     request(url, function(error,response,body){
             if(!error && response.statusCode == 200) {
-                    var results = JSON>parse(body).results[0];
+                    var results = JSON.parse(body).results[0];
                     var property_name = results.property_name; //name of hotel;
                     var lat = results.location.latitude; //lat
                     var lng = results.location.longitude; //lng
                     var price = results.total_price.amount;
                     return {name: property_name, lat: lat, lng: lng, price: price};
              }
-    }
+    });
 }
 
 
