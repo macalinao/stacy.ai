e720b3c64efa048276f13d1d4d698870fc06f61a
abhyvyth <abhyvyth@gmail.com>
2015-09-20 06:47:00 -0700

    capital one test

--- app.js
+++ app.js
@@ -29,3 +29,15 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
+
+// app.get('/capitalOne', function(req,res){
+// request('http://api.reimaginebanking.com/customers/55e94a6af8d8770528e60dcb/accounts?key=b59522ad17e95f44a3c01c23112951b3
+// ',
+//        function(error,response,body){
+//                if (!error && response.statusCode == 200) {
+//                        //var result = JSON.parse(body).results;
+//                        //var address=result[0].geometry.location;
+//                        res.send(response);
+//                }
+//        });
+// });
